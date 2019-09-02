b83cb1955e878a32c41d4fd9fb5bde2d5a30f3db
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2019-09-02 19:21:35 +0530

    Added newBtn reload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var button = document.getElementById("butt");
 var h1 = document.querySelector("h1");
 var body = document.querySelector("body");
-let div = document.querySelector("div")
+let div = document.querySelector("div");
+let newBtn = document.querySelector("#newBtn");
 let q = [];
-
 
 for (i = 0; i < 9; i++) {
     var c = document.querySelectorAll("button")
@@ -14,7 +14,7 @@ for (i = 0; i < 9; i++) {
     q[i] = c[i].style.backgroundColor = "rgb(" + x + "," + y + "," + z + ")";
 }
 let b = Math.floor(Math.random() * 9);
-var p = h1.innerHTML = q[b]; //The question header
+var p = h1.innerHTML = q[b];
 
 function result() {
     if (q[8] == p) {
@@ -33,13 +33,18 @@ function checkIndex(event) {
             c[i].style.backgroundColor = p;
             c[i].style.border = "3px solid black";
             c[i].style.pointerEvents = "none";
+            c[i].style.transition = "background 0.5s";
         }
     } else {
         c[pos].style.backgroundColor = "transparent";
         c[pos].style.border = "none";
         c[pos].style.cursor = "default";
-        c[pos].style.animation = "f";
+        c[pos].style.transition = "background 0.5s";
     }
 }
 
 window.addEventListener("scroll", () => window.scrollTo(0, 0));
+
+function reload() {
+    location.reload();
+}
